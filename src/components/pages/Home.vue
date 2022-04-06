b5bbd24f62949a9359b47cb20fb9af62664e704e
yaroofie <yaroofie@gmail.com>
2022-04-06 05:07:20 -0700

    added tailwindcss and fontawesome and cleared up the project

---
+++ src/components/pages/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1 class="text-3xl font-bold underline text-center">
+    Hello world!
+    <i class="mx-2 fa-solid fa-user-secret" />
+  </h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
